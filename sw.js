a9c23e59eafb17b3360e00943cd8cc7d7be8aec0
yatzy200011-spec <yatzy200011@gmail.com>
2026-08-20 05:05:41 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE = "papets-v66";
+const FILES = ["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
+self.addEventListener('install', e => {e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)))});
+self.addEventListener('fetch', e => {e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)))});
